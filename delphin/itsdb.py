335750137617c8add52c7b74b219420f516a3189
Michael Wayne Goodman <goodman.m.w@gmail.com>
2014-06-24 16:16:55 +0800

    doe.py now uses [incr tsdb()] profiles.
    This change allows [incr tsdb()] profiles to be used as input
    to doe.py with the `-i` option. The `--select`, `--apply`, and
    `--filter` options can customize how the profiles are read. These
    changes resulted in improvements to the `itsdb.TsdbProfile` class
    such that it can take multiple filters or applicators that are
    used when reading a table (with the read_table() method).

--- delphin/itsdb.py
+++ delphin/itsdb.py
@@ -2,7 +2,7 @@ import os
 import re
 import gzip
 import logging
-from collections import defaultdict, namedtuple
+from collections import defaultdict, namedtuple, OrderedDict
 from delphin._exceptions import ItsdbError
 
 ##############################################################################
@@ -73,7 +73,7 @@ def decode_row(line):
 
 
 def encode_row(fields):
-    return _field_delimiter.join(map(escape, fields))
+    return _field_delimiter.join(map(escape, map(str, fields)))
 
 
 def escape(string):
@@ -101,7 +101,7 @@ def _write_table(profile_dir, table_name, rows, fields,
     tbl_filename = os.path.join(profile_dir, table_name)
     mode = 'a' if append else 'w'
     if gzip:
-        mode += 't' # text mode for gzip
+        mode += 't'  # text mode for gzip
         f = gzip.open(tbl_filename + '.gz', mode=mode)
     else:
         f = open(tbl_filename, mode=mode)
@@ -124,23 +124,81 @@ def default_value(fieldname, datatype):
     else:
         return _default_datatype_values.get(datatype, '')
 
+
+def filter_rows(filters, rows):
+    for row in rows:
+        if all(condition(row, row.get(col))
+               for (cols, condition) in filters
+               for col in cols):
+            yield row
+
+
+def apply_rows(applicators, rows):
+    for row in rows:
+        for (cols, function) in applicators:
+            for col in (cols or []):
+                value = row.get(col, '')
+                row[col] = function(row, value)
+        yield row
+
+
 ##############################################################################
 # Profile class
 
 class TsdbProfile:
+    """
+    A [incr tsdb()] profile, analyzed and ready for reading or writing.
 
-    def __init__(self, root_directory):
-        """
-        Given the directory of a [incr tsdb()] profile, analyze the
-        database structure and prepare for reading.
+    Args:
+      path: the path of the directory containing the profile
+      filters: A list of tuples [(table, cols, condition)] such that only rows
+        in table where condition(row) evaluates to a non-false value are
+        returned; filters are tested in order for a table.
+      applicators: A list of tuples [(table, cols, function)] which
+        will be used when reading rows from a table---the function will
+        be applied to the contents of the column cell in the table. For
+        each table, each column-function pair will be applied in order.
+        Applicators apply after the filters.
+    """
 
-        @param root_directory: The directory where the profile files are
-            stored.
-        """
-
-        self.root = root_directory
+    def __init__(self, path, filters=None, applicators=None):
+        self.root = path
         self.relations = get_relations(os.path.join(self.root,
                                                     _relations_filename))
+
+        self.filters = defaultdict(list)
+        self.applicators = defaultdict(list)
+
+        for (table, cols, condition) in (filters or []):
+            self.add_filter(table, cols, condition)
+
+        for (table, cols, function) in (applicators or []):
+            self.add_applicator(table, cols, condition)
+
+    def add_filter(self, table, cols, condition):
+        if table not in self.relations:
+            raise ItsdbError('Cannot add filter; table "{}" is not defined '
+                             'by the relations file.'
+                             .format(table))
+        # this is a hack, though perhaps well-motivated
+        if cols is None:
+            cols = [None]
+        self.filters[table].append((cols, condition))
+
+    def add_applicator(self, table, cols, function):
+        if table not in self.relations:
+            raise ItsdbError('Cannot add applicator; table "{}" is not '
+                             'defined by the relations file.'
+                             .format(table))
+        if cols is None:
+            raise ItsdbError('Cannot add applicator; columns not specified.')
+        fields = set(f.name for f in self.relations[table])
+        for col in cols:
+            if col not in fields:
+                raise ItsdbError('Cannot add applicator; column "{}" not '
+                                 'defined by the relations file.'
+                                 .format(col))
+        self.applicators[table].append((cols, function))
 
     def table_fields(self, table_name):
         if table_name not in self.relations:
@@ -150,13 +208,7 @@ class TsdbProfile:
             )
         return self.relations[table_name]
 
-    def read_table(self, table_name):
-        """
-        Iterate through the rows in the [incr tsdb()] table.
-        """
-        field_names = [f.name for f in self.table_fields(table_name)]
-        field_len = len(field_names)
-
+    def _open_table(self, table_name):
         tbl_filename = os.path.join(self.root, table_name)
         if os.path.exists(tbl_filename):
             f = open(tbl_filename)
@@ -167,21 +219,79 @@ class TsdbProfile:
                 'Table {} does not exist at {}(.gz)'
                 .format(table_name, tbl_filename)
             )
+        return f
 
-        for line in map(str.strip, f):
-            fields = decode_row(line)
-            if len(fields) != field_len:
-                # should this throw an exception instead?
-                logging.error('Number of stored fields ({}) '
-                              'differ from the expected number({}); '
-                              'fields may be misaligned!'
-                              .format(len(fields), field_len))
-            yield dict(zip(field_names, fields))
+    def read_raw_table(self, table_name):
+        """
+        Iterate through the rows in the [incr tsdb()] table.
+        """
 
-        f.close()
+        field_names = [f.name for f in self.table_fields(table_name)]
+        field_len = len(field_names)
+        with self._open_table(table_name) as table:
+            for line in table:
+                fields = decode_row(line)
+                if len(fields) != field_len:
+                    # should this throw an exception instead?
+                    logging.error('Number of stored fields ({}) '
+                                  'differ from the expected number({}); '
+                                  'fields may be misaligned!'
+                                  .format(len(fields), field_len))
+                row = OrderedDict(zip(field_names, fields))
+                yield row
+
+    def read_table(self, table_name):
+        """
+        Iterate through the rows in the [incr tsdb()] table, yielding
+        only rows that pass any filters, and changed by any applicators.
+        """
+        filters = self.filters[table_name]
+        applicators = self.applicators[table_name]
+        rows = self.read_raw_table(table_name)
+        return filter_rows(filters, apply_rows(applicators, rows))
+
+    def select(self, table, cols, mode='list'):
+        """
+        Return the data from the specified table and columns.
+
+        Args:
+          table: The data to read data from. Only one table is allowed.
+          cols: A list of columns to read data from. If None, returns
+                all columns in relations-file order.
+          mode: How to return the data. Options:
+
+            ==============  ==================  ========================
+                 mode          description      example ['i-id', 'i-wf']
+            ==============  ==================  ========================
+            list (default)  a list of values    [10, 1]
+            dict            col to value map    {'i-id':'10','i-wf':'1'}
+            row             [incr tsdb()] row   '10@1'
+            ==============  ==================  ========================
+
+        Returns:
+          A generator of data for each row in table. The data structure
+          is specified by the `mode` parameter.
+        """
+        mode = mode.lower()
+        if mode == 'list':
+            cast = lambda cols, data: data
+        elif mode == 'dict':
+            cast = lambda cols, data: dict(zip(cols, data))
+        elif mode == 'row':
+            cast = lambda cols, data: encode_row(data)
+        else:
+            raise ItsdbError('Invalid mode for select operation: {}\n'
+                             '  Valid options include: list, dict, row'
+                             .format(mode))
+        if cols is None:
+            cols = [c.name for c in self.relations[table]]
+        for row in self.read_table(table):
+            data = [row.get(c) for c in cols]
+            yield cast(cols, data)
 
     def write_table(self, table_name, rows, append=False, gzip=False):
-        _write_table(table_name,
+        _write_table(self.root,
+                     table_name,
                      rows,
                      self.fields(table_name),
                      append=append,
--- doe.py
+++ doe.py
@@ -9,11 +9,10 @@ import re
 from collections import OrderedDict
 from copy import deepcopy
 from importlib import import_module
-#from delphin.codecs import itsdb
-#import delphin.interfaces
-#from delphin.utils import load_config
+from delphin import itsdb, mrs
 
 _rcfilename = '.pydelphinrc'
+
 
 def main(args):
     config = load_config(args)
@@ -25,13 +24,20 @@ def main(args):
         list_commands(doecfg)
         sys.exit()
 
-    prepare_output_directory(config)
-    in_profile = itsdb.load_profile()
-    for command in args.commands:
-        out_profile = 
-        cmd = make_command(command, doecfg)
-        interface = get_interface(cmd['interface'])
-        interface.do(cmd)
+    in_profile = prepare_input_profile(doecfg)
+    prepare_output_profile(config)
+
+    if args.select:
+        print(args.select)
+        table, cols = get_data_specifier(args.select)
+        print('\n'.join(in_profile.select(table, cols, mode='row')))
+
+    #for command in args.commands:
+    #    out_profile = None
+    #    cmd = make_command(command, doecfg)
+    #    interface = get_interface(cmd['interface'])
+    #    interface.do(cmd)
+
 
 def load_config(args):
     """
@@ -50,32 +56,35 @@ def load_config(args):
     elif os.path.exists(os.path.join(os.getcwd(), _rcfilename)):
         args.config = os.path.join(os.getcwd(), _rcfilename)
     elif os.path.exists(os.path.join(os.environ['HOME'], _rcfilename)):
-        args.config = os.path.join(args.output_directory, _rcfilename)
+        args.config = os.path.join(args.output_profile, _rcfilename)
     else:
         sys.exit('No config file found. Exiting.')
 
     logging.debug('Loading the config file found at {}'.format(args.config))
     config = json.loads(strip_json_comments(open(args.config,'r')),
                         object_pairs_hook=OrderedDict)
-    doecfg = config['doe']
+    cfg = config['doe']
 
     # override the loaded values with those from the command-line
-    if 'variables' not in doecfg:
-        doecfg['variables'] = {}
+    cfg.update({
+        'verbosity': args.verbosity,
+        'input_profile': args.input_profile or cfg.get('input_profile'),
+        'output_profile': args.output_profile or cfg.get('output_profile'),
+        'select': args.select or cfg.get('select'),
+        'apply': args.apply or cfg.get('apply', []),
+        'filter': args.filter or cfg.get('filter', []),
+    })
+    # these overrides need some nesting, so do them separate
+    if 'variables' not in cfg:
+        cfg['variables'] = {}
     if args.source_grammar:
-        doecfg['variables']['source_grammar'] = args.source_grammar
+        cfg['variables']['source_grammar'] = args.source_grammar
     if args.transfer_grammar:
-        doecfg['variables']['transfer_grammar'] = args.transfer_grammar
+        cfg['variables']['transfer_grammar'] = args.transfer_grammar
     if args.target_grammar:
-        doecfg['variables']['target_grammar'] = args.target_grammar
+        cfg['variables']['target_grammar'] = args.target_grammar
     if args.grammar:
-        doecfg['variables']['grammar'] = args.grammar
-    if args.output_directory:
-        doecfg['output_directory'] = args.output_directory
-    if args.input_profile:
-        doecfg['input_profile'] = args.input_profile
-    if args.verbosity:
-        doecfg['verbosity'] = args.verbosity
+        cfg['variables']['grammar'] = args.grammar
 
     return config
 
@@ -95,20 +104,21 @@ def strip_json_comments(fh):
             lines.append(line)
     return ''.join(lines)
 
+
 def validate(args, doecfg):
     """ Validate arguments not caught by argparse. """
     valid = True
     if not args.list:
-        if len(args.commands) == 0:
-            logging.error('At least one command is required.')
-            valid = False
-        if args.output_directory is None:
-            logging.error('An --output-directory argument is required.')
+        #if len(args.commands) == 0:
+        #    logging.error('At least one command is required.')
+        #    valid = False
+        if args.output_profile is None:
+            logging.error('An --output-profile argument is required.')
             valid = False
         if args.input_profile is None:
             logging.error('An --input-profile argument is required.')
             valid = False
-    return valid 
+    return valid
 
 
 def list_commands(config):
@@ -116,11 +126,26 @@ def list_commands(config):
     for c, cmd in config['commands'].items():
         print('  {}: {}'.format(c, cmd.get('description')))
 
-def prepare_output_directory(config):
+
+def prepare_input_profile(cfg):
+    prof = itsdb.TsdbProfile(cfg['input_profile'])
+    print(cfg.get('filter'))
+    for spr, f in cfg.get('filter', []):
+        table, cols = get_data_specifier(spr)
+        f = make_lambda_function(f)
+        prof.add_filter(table, cols, f)
+    for spr, f in cfg.get('apply', []):
+        table, cols = get_data_specifier(spr)
+        f = make_lambda_function(f)
+        prof.add_applicator(table, cols, f)
+    return prof
+
+
+def prepare_output_profile(config):
     #if not config.get('output_directory'):
     #    config['output_directory'] = os.getcwd()
     doecfg = config['doe']
-    outdir = doecfg['output_directory']
+    outdir = doecfg['output_profile']
     try:
         os.makedirs(outdir, exist_ok=True)
     except PermissionError:
@@ -133,7 +158,7 @@ def prepare_output_directory(config):
         sys.exit(1)
     # copy configuration for documentation
     # TODO: allow for multiple configs in same directory (e.g. (PID).rcfile)
-    rcpath = os.path.join(doecfg['output_directory'], _rcfilename)
+    rcpath = os.path.join(doecfg['output_profile'], _rcfilename)
     if not os.path.exists(rcpath):
         json.dump(config, open(rcpath, 'w'), indent=2)
     else:
@@ -155,32 +180,117 @@ def make_command(command, doecfg):
         cmd['grammar'] = doecfg['grammar']
     return cmd
 
+
 def get_interface(interface_name):
     return import_module('{}.{}'.format('delphin.interfaces', interface_name))
 
+
+data_specifier_re = re.compile(r'(?P<table>[^:]+)(:(?P<cols>.+))?$')
+def get_data_specifier(string):
+    """
+    Return a tuple (table, col) for some [incr tsdb()] data specifier.
+    For example:
+
+      item              -> ('item', None)
+      item:i-input      -> ('item', ['i-input'])
+      item:i-input@i-wf -> ('item', ['i-input', 'i-wf'])
+      (otherwise)       -> (None, None)
+    """
+    match = data_specifier_re.match(string)
+    if match is None:
+        return (None, None)
+    table = match.group('table').strip()
+    cols = match.group('cols')
+    if cols is not None:
+        cols = list(map(str.strip, cols.split('@')))
+    return (table, cols)
+
+
+def make_lambda_function(function):
+    return eval('lambda row, x:{}'.format(function))
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('pyDelphin Operating Environment')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='''
+DATA SPECIFIERS
+  The --select, --apply, and --filter options take as a first argument
+  a string to specify the table and, if desired or necessary, the column
+  from a [incr tsdb()] profile to draw data from. These take the form:
+    TABLE[:COL[@COL..]]
+  For example, this selects an entire row from the `item` table:
+    item
+  while this selects the `i-id` and `i-input` columns for each row:
+    item:i-id@i-input
+  For --select and --filter, the COL specifiers are optional. For
+  --apply, at least one COL is required.
+
+FILTER/APPLY EXPRESSIONS
+  The --filter and --apply options take as a second argument a small
+  Python expression to be evaluated. This expression is turned into a
+  lambda expression like this:
+      f = lambda row, x: EXPR
+  Thus, EXPR can use `row`, which is the contents of the entire row as a
+  Python dictionary, and `x` is the value of the current column (if COLs
+  are specified, otherwise it is None). The type of `x` is a string for
+  --apply, and it will be a string for --filter unless an --apply
+  operation has recast it. For example, these are equivalent and select
+  all rows in `item` where the `i-id` field has an integer value less
+  than 10:
+    --filter item "int(row['i-id']) < 10"
+    --filter item:i-id "int(x) < 10"
+    --apply item:i-id "int(x)" --filter item:i-id "x < 10"
+  Note, however, that the last one with the --apply operation differs in
+  that the value of `item:i-id` remains an `int` after the filtering.
+        '''
+    )
+    add = parser.add_argument
+    add('-c', '--config',
+        metavar='PATH',
+        help='The configuration file describing possible commands.')
     # the --list option is a specific kind of help that uses the config.
     # When it is not used, --outdir and a command are required, but we
     # can't use argparse to state the requirement (validate later)
-    parser.add_argument('-l', '--list', action='store_true',
-        help='list the commands allowed by the configuration')
-    parser.add_argument('-v', '--verbose', action='count', dest='verbosity',
-        default=2, help='increase the verbosity (can be repeated: -vvv)')
-    parser.add_argument('-q', '--quiet', action='store_const', const=0,
-        dest='verbosity', help='set verbosity to the quietest level')
-    parser.add_argument('-o', '--output-directory', metavar='DIR',
-        help='the output directory for all commands')
-    parser.add_argument('-i', '--input-profile', metavar='PATH',
-        help='the [incr tsdb()] profile to use as input for the first '
-             'command')
-    parser.add_argument('-c', '--config', metavar='PATH',
-        help='the configuration file describing possible commands')
-    parser.add_argument('-s', '--source-grammar', metavar='PATH')
-    parser.add_argument('-x', '--transfer-grammar', metavar='PATH')
-    parser.add_argument('-t', '--target-grammar', metavar='PATH')
-    parser.add_argument('-g', '--grammar', metavar='PATH')
-    parser.add_argument('commands', nargs='*')
+    add('-l', '--list',
+        action='store_true',
+        help='List the commands allowed by the configuration.')
+    add('-v', '--verbose',
+        action='count', dest='verbosity', default=2,
+        help='Increase the verbosity (can be repeated: -vvv).')
+    add('-q', '--quiet',
+        action='store_const', const=0, dest='verbosity',
+        help='Set verbosity to the quietest level.')
+    add('-i', '--input-profile',
+        metavar='PATH',
+        help='The [incr tsdb()] profile to use as input.')
+    add('-o', '--output-profile',
+        metavar='PATH',
+        help='The [incr tsdb()] profile to output.')
+    add('-s', '--select',
+        metavar='TBL[:COL[@COL..]]',
+        help='Print the contents of each row in TBL. If COLs are specified, '
+             'only print contents of these columns. See also the section '
+             'about DATA SPECIFIERS. e.g. '
+             '--select item:i-input@i-wf')
+    add('-a', '--apply',
+        nargs=2, metavar=('TBL:COL[@COL..]', 'EXPR'), action='append',
+        help='Apply EXPR to the contents of COL for each COL for each row '
+             'in TBL. See also the section about DATA SPECIFIERS and '
+             'FILTER/APPLY EXPRESSIONS. e.g. '
+             '--apply result:mrs "mrs.convert(x, \'mrx\', \'dmrx\')"')
+    add('-f', '--filter',
+        nargs=2, metavar=('TBL[:COL[@COL..]]', 'EXPR'), action='append',
+        help='Ignore rows from TBL where EXPR returns a non-true value for '
+             'the row. If COLs are specified, EXPR is tested for every COL '
+             'on each row. See also the section about DATA SPECIFIERS and '
+             'FILTER/APPLY EXPRESSIONS. e.g. '
+             '--filter item "int(row[\'i-length\']) < 5"')
+    add('-S', '--source-grammar', metavar='PATH')
+    add('-T', '--target-grammar', metavar='PATH')
+    add('-X', '--transfer-grammar', metavar='PATH')
+    add('-G', '--grammar', metavar='PATH')
+    add('commands', nargs='*')
 
     args = parser.parse_args()
     logging.basicConfig(level=50-(args.verbosity*10))
