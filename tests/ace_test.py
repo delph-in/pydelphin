082277afe50b06d3c765965271249249b36e517a
alvas <alvations@gmail.com>
2014-07-17 17:13:43 +0200

    added the install function in ace_test.py

--- tests/ace_test.py
+++ tests/ace_test.py
@@ -3,6 +3,7 @@
 import os.path, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 
-from ace import parse
+import ace
 
-print parse('this is a foo bar sentence.', onlyMRS=True, bestparse=True)
+ace.install()
+print ace.parse('this is a foo bar sentence.', onlyMRS=True, bestparse=True)
