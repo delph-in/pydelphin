21c8babb02185c1f32f0fc2eb2e22ef41a134248
Michael Wayne Goodman <goodman.m.w@gmail.com>
2014-10-19 12:23:58 +0800

    Add ACE interface.

--- delphin/interfaces/ace.py
+++ delphin/interfaces/ace.py
@@ -1,100 +1,182 @@
 
-"""ACE interface for DOE commands"""
+"""ACE interface"""
 
 import logging
-from subprocess import Popen, PIPE, STDOUT
-from .util import decode_stagfile, encode_stagfile
-from delphin.interfaces import _do
+from subprocess import (check_call, CalledProcessError, Popen, PIPE, STDOUT)
 
-def do(cmd):
-    # validate cmd here (e.g. that it has a 'grammar' key, correct 'task', etc)
-    task = cmd['task']
-    grammar = cmd['grammar']
-    cmdargs = cmd['arguments'] + ['-g', grammar]
-    if task == 'parse':
-        process_output = parse_results
-    elif task == 'transfer':
-        process_output = transfer_results
-    elif task == 'generate':
-        process_output = generation_results
-    else:
-        logging.error('Task "{}" is unsupported by the ACE interface.'
-                      .format(task))
-        return
-    cmdargs = map(lambda a: a.format(**cmd['variables']), cmdargs)
-    _do()
+class AceProcess(object):
 
-def process(cmd, infile, outfile, config):
-    c = config['commands'][cmd]
-    ct = config['commandtypes'][c['type']]
-    cmdargs = map(lambda a: a.format(**config['variables']), c['arguments'])
-    # try...except this
-    task = ct['task']
-    key = ct['input_key']
-    if task == 'parse':
-        parse_output = parse_results
-    elif task == 'generate':
-        parse_output = generation_results
-    elif task == 'transfer':
-        parse_output = transfer_results
-    else:
-        pass #log this
-    outstream = open(outfile, 'w')
-    with Popen(cmdargs, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
-               universal_newlines=True) as ace:
-        for item_id, provenance, data in decode_stagfile(open(infile,'r')):
-            indata = data.get(key)
-            if indata is None: continue
-            ace.stdin.write(indata + '\n')
-            ace.stdin.flush()
-            for result_id, result in parse_output(ace.stdout):
-                print(encode_stagfile(
-                        item_id,
-                        provenance + ['{}:{}'.format(cmd, result_id)],
-                        result),
-                      file=outstream)
-    outstream.close()
+    _cmdargs = []
+
+    def __init__(self, grm, cmdargs=None):
+        self.grm = grm
+        self.cmdargs = cmdargs or []
+        self._p = Popen(
+            ['ace', '-g', self.grm] + self._cmdargs + self.cmdargs,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=STDOUT,
+            universal_newlines=True
+        )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+        return False  # don't try to handle any exceptions
+
+    def send(self, datum):
+        self._p.stdin.write(datum.rstrip() + '\n')
+        self._p.stdin.flush()
+
+    def receive(self):
+        return self._p.stdout
+
+    def interact(self, datum):
+        self.send(datum)
+        result = self.receive()
+        return result
+
+    def read_result(self, result):
+        return result
+
+    def close(self):
+        self._p.stdin.close()
+        for line in self._p.stdout:
+            logging.debug('ACE cleanup: {}'.format(line.rstrip()))
+        retval = self._p.wait()
+        return retval
 
 
-def parse_results(instream):
-    item = {}
-    status = None
-    counter = 0
-    line = instream.readline()
-    while line:
-        if line.startswith('SENT:'):
-            pass
-        elif line.startswith('SKIP:'):
-            pass
-        elif line.startswith('WARNING:'):
-            pass
-        elif line.startswith('ERROR:'):
-            pass
-        elif line.startswith('NOTE:'):
-            status = 'NOTE'
-        elif line.strip() == '':
-            if status == 'BLANK':
-                break
+class AceParser(AceProcess):
+
+    def __init__(self, *args, **kwargs):
+        AceProcess.__init__(self, *args, **kwargs)
+
+    def receive(self):
+        response = {
+            'NOTES': [],
+            'WARNINGS': [],
+            'ERRORS': [],
+            'SENT': None,
+            'RESULTS': []
+        }
+
+        failed = False
+        blank = 0
+
+        stdout = self._p.stdout
+        line = stdout.readline().rstrip()
+        if line.startswith('NOTE: '):
+            failed = True
+        while True:
+            if line.strip() == '':
+                blank += 1
+                if blank >= 2:
+                    break
+            elif line.startswith('SENT: ') or line.startswith('SKIP: '):
+                response['SENT'] = line.split(': ', 1)[1]
+            elif (line.startswith('NOTE:') or
+                  line.startswith('WARNING') or
+                  line.startswith('ERROR')):
+                level, message = line.split(': ', 1)
+                response['{}S'.format(level)].append(message)
             else:
-                status = 'BLANK'
-        else:
-            mrs, dtree = line.split(' ; ')
-            yield (counter, {'mrs': mrs.strip(), 'derivation': dtree.strip()})
-            counter += 1
-        line = instream.readline()
+                mrs, deriv = line.split(' ; ')
+                response['RESULTS'].append({
+                    'MRS': mrs.strip(),
+                    'DERIV': deriv.strip()
+                })
+            line = stdout.readline().rstrip()
+        #if not failed:
+        #    line = stdout.readline().rstrip()
+        #    assert line.startswith('NOTE:')
+        #    response['NOTES'].append(line.replace('NOTE: ', '', 1))
+        return response
 
-def generation_results(instream):
-    item = {}
-    status = None
-    counter = 0
-    line = instream.readline()
-    while line:
-        if line.startswith('NOTE:'):
-            break
-        else:
-            yield (counter, {'sentence': line.strip()})
-            counter += 1
-        line = instream.readline()
 
-def format_input(data, field):
-    return data.get(field, '')
+class AceGenerator(AceProcess):
+
+    _cmdargs = ['-e']
+
+    def receive(self):
+        response = {
+            'NOTE': None,
+            'WARNING': None,
+            'ERROR': None,
+            'SENT': None,
+            'RESULTS': None
+        }
+        results = []
+
+        stdout = self._p.stdout
+        line = stdout.readline().rstrip()
+        while not line.startswith('NOTE: '):
+            if line.startswith('WARNING') or line.startswith('ERROR'):
+                level, message = line.split(': ', 1)
+                response[level] = message
+            else:
+                results.append(line)
+            line = stdout.readline().rstrip()
+        # sometimes error messages aren't prefixed with ERROR
+        if line.endswith('[0 results]') and len(results) > 0:
+            response['ERROR'] = '\n'.join(results)
+            results = []
+        response['RESULTS'] = results
+        return response
+
+
+def compile(cfg_path, out_path, log=None):
+    #debug('Compiling grammar at {}'.format(abspath(cfg_path)), log)
+    try:
+        check_call(
+            ['ace', '-g', cfg_path, '-G', out_path],
+            stdout=log, stderr=log, close_fds=True
+        )
+    except (CalledProcessError, OSError):
+        logging.error(
+            'Failed to compile grammar with ACE. See {}'
+            .format(abspath(log.name) if log is not None else '<stderr>')
+        )
+        raise
+    #debug('Compiled grammar written to {}'.format(abspath(out_path)), log)
+
+
+def parse_from_iterable(cfg_path, data):
+    with AceParser(cfg_path) as parser:
+        for datum in data:
+            yield parser.interact(datum)
+
+
+def parse(cfg_path, datum):
+    return next(parse_from_iterable(cfg_path, [datum]))
+
+
+def generate_from_iterable(cfg_path, data):
+    with AceGenerator(cfg_path) as generator:
+        for datum in data:
+            yield generator.interact(datum)
+
+
+def generate(cfg_path, datum):
+    return next(generate_from_iterable(cfg_path, [datum]))
+
+
+# def do(cmd):
+#     # validate cmd here (e.g. that it has a 'grammar' key, correct 'task', etc)
+#     task = cmd['task']
+#     grammar = cmd['grammar']
+#     cmdargs = cmd['arguments'] + ['-g', grammar]
+#     if task == 'parse':
+#         process_output = parse_results
+#     elif task == 'transfer':
+#         process_output = transfer_results
+#     elif task == 'generate':
+#         process_output = generation_results
+#     else:
+#         logging.error('Task "{}" is unsupported by the ACE interface.'
+#                       .format(task))
+#         return
+#     cmdargs = map(lambda a: a.format(**cmd['variables']), cmdargs)
+#     _do()
