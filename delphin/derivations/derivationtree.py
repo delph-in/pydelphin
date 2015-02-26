4803510c6d79472ea78d2f3bbecd6be55bd66fad
T.J. Trimble <trimblet@me.com>
2015-02-25 20:06:18 -0800

    Working Derivation class for manipulating ACE derivations
    NOTE: edited by @goodmami (Michael Wayne Goodman) to remove the ACE interface changes; I'm just cherry-picking the derivation files.

--- delphin/derivations/derivationtree.py
+++ delphin/derivations/derivationtree.py
@@ -7,45 +7,151 @@ class Derivation(object):
     @author: T.J. Trimble
     """
 
-    ace_tree_compile = re.compile(r"#T\[(?P<EDGE_ID>\d+) (?P<LABEL>\"?\w+\"?) (?P<TOKEN>\"\w+\"|nil) (?P<CHART_ID>\d+) (?P<RULE_NAME>\w+)\W*(?P<CHILDREN>#T\[.*?\])*\W*\]")
-    ace_children = re.compile(r"#T\[.*?\]")
+    quotes = re.compile("\'|\"")
 
+    ace_pattern = " ".join(("(?P<EDGE_ID>\d+)",
+                            "(?P<LABEL>{0}[\w{1}]+{0})",
+                            "(?P<TOKEN>({0}[\w{1}]+{0}|nil))",
+                            "(?P<CHART_ID>\d+)",
+                            "(?P<RULE_NAME>[\w{1}]+)",)).format("[\'\"]?", re.escape("-+=/:.!?$<>@#%^&*"))
 
-    def __init__(self, representation):
+    def __init__(self, definition, data_package=None, children=None):
         """
-        For now, assume ACE output
-        """
-        self.read_ACE(representation)
+        Two constructors:
+            Derivation(str:definition):
+                Construct a Derivation object from a bracketed tree structure.
+                ex. Derivation("(S (NP (N I)) (VP (V fell)))")
 
+            Derivation(None, tuple:data_package, list:children):
+                Construct a Derivation object from tuple of values and a list of children.
+                The contents of data_package are:
+                    edge_ID, label, token, chart_ID, rule_name, [tree_ID]
+                ex. Derivation(None, data_package=(...), children=[])
+        """
+        if definition:
+            tree = Derivation.read_ACE(definition)
+            data_package = (tree.edge_ID, tree.label, tree.token, tree.chart_ID, tree.rule_name, tree.tree_ID)
+            children = tree.children
+        if children is None:
+            raise TypeError("%s: Expected a node value and child list "
+                            % type(self).__name__)
+        elif isinstance(children, str):
+            raise TypeError("%s() argument 2 should be a list, not a "
+                            "string" % type(self).__name__)
+        else:
+            if len(data_package) not in (5, 6):
+                raise ValueError("%s() data_package argument must be a "
+                                 "tuple of length 5" % self.__class__.__name__)
+        try:
+            self.children = children
+            self.edge_ID = data_package[0]
+            self.label = Derivation.quotes.sub("", data_package[1])
+            self.token = Derivation.quotes.sub("", data_package[2]) if data_package[2] not in (None, "nil") else None
+            self.chart_ID = data_package[3]
+            self.rule_name = data_package[4]
+            self.tree_ID = data_package[5] if len(data_package) >= 6 else None
+        except Exception as e:
+            print(data_package)
+                
     # Interface specific methods
     #  Maybe move these to their respective interface classes?
-    def read_ACE(self, representation):
-        # '#T[EDGE_ID "LABEL" ("TOKEN"|nil) CHART_ID RULE_NAME [#T[]]]'
-        error = ValueError("Derivation Tree is malformed: {}".format(representation))
-        result = Derivation.ace_tree_compile.match(representation)
-        # print("\n")
-        # print(representation)
-        # print(result)
-        # print(result.groups())
-        if not result:
-            raise error
-        try:
-            self.edge_ID = result.group('EDGE_ID')
-            self.label = result.group('LABEL').strip('"')
-            self.token = result.group('TOKEN').strip('"') if result.group('TOKEN') != "nil" else None
-            self.chart_ID = result.group('CHART_ID')
-            self.rule_name = result.group('RULE_NAME')
-            # Read in children
-            children = result.group('CHILDREN') or "" # defaults
-            children = Derivation.ace_children.findall(children)
-            #print(children)
-            if children:
-                self.children = [Derivation(child) for child in children]
+
+    @classmethod
+    def read_ACE(cls, s):
+        """
+        Based on NLTK's Tree.fromstring()
+
+        Input Format:
+            tree 1 #T[1 'S' nil 3 subj-head #T[2 'N' 'I' 93 i] #T[2 'V' 'run' 93 run_v1]] "I run"
+        """
+        s = s.strip()
+        # Parse introduction
+        if s.startswith("tree "):
+            _, tree_ID, s = s.split(None, 2)
+            if s.endswith('"'): # Only do this if there's a quote at the end
+                s = s.rsplit('"', 2)[0].strip() # assumes no quote in text
+        else:
+            tree_ID = None
+        # Initialize bracket pointers        
+        open_b = "#T["
+        close_b = "]"
+        open_pattern, close_pattern = (re.escape(open_b), re.escape(close_b))
+        # Leaves and nodes contain non-whitespace, non-bracket characters
+        valid_chars = '[^\s%s%s]+' % (open_pattern, close_pattern)
+        bracket_patterns = '[%s%s]+' % (open_pattern, close_pattern)
+        # Construct a regexp that will tokenize the string.
+        token_re = re.compile('%s%s|%s|(%s)' % (
+            open_pattern, Derivation.ace_pattern, close_pattern, valid_chars))
+        # Walk through each token, updating a stack of trees.
+        stack = [(None, [])] # list of (node, children) tuples
+        tokens = list(token_re.finditer(s))
+        for i, match in enumerate(tokens):
+        #for match in token_re.finditer(s):
+            token = match.group()
+            # Definition of a tree/subtree
+            #  #T[EDGE_ID LABEL TOKEN CHART_ID RULE_NAME
+            if token.startswith(open_b):
+                if len(stack) == 1 and len(stack[0][1]) > 0:
+                    cls._parse_error(s, match, 'end-of-string')
+                edge_ID = match.group('EDGE_ID')
+                label = match.group('LABEL')
+                token = match.group('TOKEN')
+                chart_ID = match.group('CHART_ID')
+                rule_name = match.group('RULE_NAME')
+                stack.append(((edge_ID, label, token, chart_ID, rule_name), []))
+            # End of a tree/subtree
+            elif token == close_b:
+                if len(stack) == 1:
+                    if len(stack[0][1]) == 0:
+                        cls._parse_error(s, match, open_b)
+                    else:
+                        cls._parse_error(s, match, 'end-of-string')
+                data, children = stack.pop()
+                stack[-1][1].append(cls(None, data_package=data, children=children))
+            # Leaf node
             else:
-                self.children = []
-            #print("\n")
-        except IndexError:
-            raise error
+                if len(stack) == 1:
+                    cls._parse_error(s, match, open_b)
+                stack[-1][1].append(token)
+
+        # check that we got exactly one complete tree.
+        if len(stack) > 1:
+            cls._parse_error(s, 'end-of-string', close_b)
+        elif len(stack[0][1]) == 0:
+            cls._parse_error(s, 'end-of-string', open_b)
+        else:
+            assert stack[0][0] is None
+            assert len(stack[0][1]) == 1
+        tree = stack[0][1][0]
+
+        # Get tree ID
+        tree.tree_ID = tree_ID
+        # return the tree
+        return tree
+
+    @classmethod
+    def _parse_error(cls, s, match, expecting):
+        """
+        From NLTK: http://www.nltk.org/_modules/nltk/tree.html
+        """
+        # Construct a basic error message
+        if match == 'end-of-string':
+            pos, token = len(s), 'end-of-string'
+        else:
+            pos, token = match.start(), match.group()
+        msg = '%s.read(): expected %r but got %r\n%sat index %d.' % (
+            cls.__name__, expecting, token, ' '*12, pos)
+        # Add a display showing the error token itsels:
+        s = s.replace('\n', ' ').replace('\t', ' ')
+        offset = pos
+        if len(s) > pos+10:
+            s = s[:pos+10]+'...'
+        if pos > 10:
+            s = '...'+s[pos-10:]
+            offset = 13
+        msg += '\n%s"%s"\n%s^' % (' '*16, s, ' '*(17+offset))
+        raise ValueError(msg)
+
 
     # Core methods
     def __eq__(self, other):
@@ -71,12 +177,36 @@ class Derivation(object):
         # Return true if they're the same!
         return True
 
+    ## Output methods
+    # Standard methods
+    def _output(self, format_string="[{LABEL}{TOKEN}{RULE}{CHILDREN}]"):
+        format_map = {
+            "EDGE_ID": " "+self.edge_ID,
+            "LABEL": " "+self.label if self.label else "XP",
+            "TOKEN": " "+self.token if self.token else "",
+            "CHART_ID": " "+self.chart_ID,
+            "RULE": " "+self.rule_name if self.rule_name else "",
+            "CHILDREN": " {}".format(" ".join(child._output(format_string=format_string) for child in self.children)) if self.children else "",
+        }
+        return format_string.format(**format_map)
+
     def __str__(self):
-        # TODO: this
-        return self.get_HTML()
+        return self._output()
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.__class__.__name__, str(self))
+
+    def output_ACE(self):
+        """
+        Return ACE-like derivation string. Format:
+            #T[{EDGE_ID}{LABEL}{TOKEN}{CHART_ID}{RULE}{CHILDREN}]
+        
+        where values are joined by spaces.
+        """
+        return self._output(format_string="#T[{EDGE_ID}{LABEL}{TOKEN}{CHART_ID}{RULE}{CHILDREN}]")
 
     # HTML Methods
-    def get_HTML(self, title_text=True):
+    def output_HTML(self, title_text=True):
         """
         Returns HTML representation of tree in the following format:
             <div id=CHART_ID title="CHART_ID: RULE_NAME"><p>LABEL</p>(<p>TOKEN</p>)?</div>
@@ -93,7 +223,7 @@ class Derivation(object):
             "LABEL": self.label,
             "TOKEN": "<p>{}</p>".format(self.token) if self.token else "",
             "TITLE": " title=\"{}: {}\"".format(self.chart_ID, self.rule_name) if title_text else "",
-            "CHILDREN": "".join(child.get_HTML(title_text=title_text) for child in self.children)
+            "CHILDREN": "".join(child.output_HTML(title_text=title_text) for child in self.children)
         }
         # Return result
         return result.format(**values)
@@ -110,3 +240,114 @@ class Derivation(object):
 
     def dumps(self):
         pass
+
+
+
+#### ARCHIVE ####
+    # def _parse_definition(self, pointer, i, representation):
+    #     """
+    #     Parses ACE output definition
+    #     """
+    #     print("\nREPRESENTATION: " + str(representation))
+    #     definition = representation[pointer:i].split(None, 5)
+    #     print("\nDEFINITION: " + str(definition))
+    #     try:
+    #         self.edge_ID, self.label, self.token, self.chart_ID, self.rule_name = definition[:5]
+    #     except ValueError as e: # TODO: this
+    #         raise e
+
+    # def read_ACE(self, representation):
+    #     # 'TREE_ID #T[EDGE_ID "LABEL" ("TOKEN"|nil) CHART_ID RULE_NAME [#T[]]]'
+    #     # 'No parse found for X'
+    #     if representation.startswith('No parse found for '):
+    #         self.no_parse = True
+    #         return
+    #     print("NEW TREE! parent: " + str(self.parent or "None"))
+    #     representation = representation.strip()
+    #     self.children = []
+    #     # Parse representation
+    #     self.pointer = 2 if representation.startswith("#T[") else 0 # base case
+    #     length = len(representation)
+    #     i = 0
+    #     while True:
+    #         c = representation[i]
+    #         print(c, end=" ")
+    #         if c == "[":
+    #             print("\n[ at %s, pointer is at %s" % (str(i), str(self.pointer)))
+    #             if i != self.pointer: # avoids base case
+    #                 # Found a definition
+    #                 self._parse_definition(self.pointer, i, representation)
+    #                 print("REST: " + str(representation[i:]))
+    #                 child = Derivation(representation[i:], parent=self)
+    #                 print(" CHILD %s -> %s" % (child.label, self))
+    #                 self.children.append(child)
+    #                 i += child.pointer+4 # Hop over parsed child
+    #                 # find next bracket
+    #                 self.pointer = i-1
+    #                 print("Jumped to: " + str(i))
+    #             else:
+    #                 self.pointer = i+1 # base case
+    #         elif c == "]":
+    #             print("\n] at " + str(i)) 
+    #             # terminal case
+    #             if self.pointer == 1:
+    #                 self._parse_definition(self.pointer, i, representation)
+    #             self.pointer = i # Mark position of end bracket
+    #             return # stop parsing
+    #         i += 1
+    #         if i >= len(representation):
+    #             raise ValueError("Unbalanced parentheses!") # TODO: this
+        
+    # def read_ACE(self, representation):
+    #     # 'TREE_ID #T[EDGE_ID "LABEL" ("TOKEN"|nil) CHART_ID RULE_NAME [#T[]]]'
+    #     # 'No parse found for X'
+    #     representation = representation.strip()
+    #     if representation.startswith('No parse found for '):
+    #         self.no_parse = True
+    #     else:
+    #         # Parse metadata
+    #         match = Derivation.ace_tree.match(representation)
+    #         self.tree_ID = match.group('TREE_ID') or None
+    #         self.sent = match.group('SENT') or None
+    #         tree = match.group('TREE')[3:-1] # strip "#T[.*]"
+    #         # Parse tree into memory
+    #         tree = tree.split(None, 5)
+    #         print(tree)
+    #         self.edge_ID = tree[0]
+    #         self.label = tree[1].strip('"')
+    #         self.token = tree[2] if tree[2] != "nil" else None
+    #         self.chart_ID = tree[3]
+    #         self.rule_name = tree[4]
+    #         if len(tree) >= 6:
+    #             children = Derivation.ace_children.findall(tree[5])
+    #             print("REGEX: " + str(children))
+    #             self.children = [Derivation(child) for child in children]
+    #             print(self.children)
+
+
+    # def read_ACE(self, representation):
+    #     # 'TREE_ID #T[EDGE_ID "LABEL" ("TOKEN"|nil) CHART_ID RULE_NAME [#T[]]]'
+    #     # 'No parse found for X'
+    #     error = ValueError("Derivation Tree is malformed: {}".format(representation))
+    #     result = Derivation.ace_tree_compile.match(representation)
+    #     if not result:
+    #         raise error
+    #     elif result.group('NO_PARSE'):
+    #         self.no_parse = True
+    #         return
+    #     try:
+    #         self.tree_ID = result.group('TREE_ID') or None
+    #         self.edge_ID = result.group('EDGE_ID')
+    #         self.label = result.group('LABEL').strip('"')
+    #         self.token = result.group('TOKEN').strip('"') if result.group('TOKEN') != "nil" else None
+    #         self.chart_ID = result.group('CHART_ID')
+    #         self.rule_name = result.group('RULE_NAME')
+    #         # Read in children
+    #         children = result.group('CHILDREN') or "" # defaults
+    #         children = Derivation.ace_children.findall(children)
+    #         if children:
+    #             self.children = [Derivation(child) for child in children]
+    #         else:
+    #             self.children = []
+    #     except IndexError:
+    #         raise error
--- tests/derivationtree_test.py
+++ tests/derivationtree_test.py
@@ -5,12 +5,15 @@ import unittest
 class derivationtree_test(unittest.TestCase):
 
     def setUp(self):
+        # ACE output text
+        self.ace_output_text = "tree 1 #T[1 \"LABEL\" \"TOKEN\" 2 RULE_NAME]"
+        self.ace_output_text2 = "tree 1 #T[1 \"LABEL\" \"TOKEN\" 2 RULE_NAME] \"TOKEN\""
         # Terminal text
-        self.terminal_text = "#T[1 \"LABEL\" \"TOKEN\" 2 RULE_NAME]'"
-        self.terminal_text2 = "#T[10 \"LABEL\" \"TOKEN\" 34 RULE_NAME]'"
-        self.terminal_text3 = "#T[485 \"LABEL\" \"TOKEN\" 9238 RULE_NAME]'"
-        self.terminal_nil = "#T[1 \"LABEL\" nil 2 RULE_NAME]'"
-        self.terminal_grammar_pred = "#T[1 LABEL \"TOKEN\" 2 RULE_NAME]'"
+        self.terminal_text = "#T[1 \"LABEL\" \"TOKEN\" 2 RULE_NAME]"
+        self.terminal_text2 = "#T[10 \"LABEL\" \"TOKEN\" 34 RULE_NAME]"
+        self.terminal_text3 = "#T[485 \"LABEL\" \"TOKEN\" 9238 RULE_NAME]"
+        self.terminal_nil = "#T[1 \"LABEL\" nil 2 RULE_NAME]"
+        self.terminal_grammar_pred = "#T[1 LABEL \"TOKEN\" 2 RULE_NAME]"
         # Unary text
         self.unary_text = "#T[9 \"LABEL\" nil 93 RULE_NAME #T[10 \"LABEL2\" \"TOKEN\" 39 RULE_NAME2]]"
         self.unary_text2 = "#T[39 \"LABEL\" nil 93 RULE_NAME #T[40 \"LABEL2\" \"TOKEN\" 20 RULE_NAME2] ]"
@@ -23,6 +26,112 @@ class derivationtree_test(unittest.TestCase):
         self.ternary_text = "#T[9 \"LABEL\" nil 93 RULE_NAME #T[10 \"LABEL2\" \"TOKEN\" 39 RULE_NAME2] #T[11 \"LABEL3\" \"TOKEN2\" 240 RULE_NAME3] #T[12 \"LABEL4\" \"TOKEN3\" 340 RULE_NAME4]]"
         self.ternary_text2 = "#T[10 \"LABEL\" nil 94 RULE_NAME #T[11 \"LABEL2\" \"TOKEN\" 40 RULE_NAME2] #T[12 \"LABEL3\" \"TOKEN2\" 241 RULE_NAME3] #T[13 \"LABEL4\" \"TOKEN3\" 341 RULE_NAME4]]"
         self.ternary_text3 = "#T[16 \"LABEL\" nil 95 RULE_NAME #T[12 \"LABEL2\" \"TOKEN\" 41 RULE_NAME2] #T[13 \"LABEL3\" \"TOKEN2\" 244 RULE_NAME3] #T[16 \"LABEL4\" \"TOKEN3\" 342 RULE_NAME4]]"
+        # Nested text
+        self.nested_text = "#T[1 'S' nil 3 subj-head #T[2 'NP' nil 93 spec-head #T[2 'D' 'the' 93 the_d1] #T[3 'N' 'man' 39 man_n1]] #T[3 'V' 'runs' 39 runs_v1]]"
+        self.nested_high_count = "#T[10 'S' nil 3 subj-head #T[20 'NP' nil 93 spec-head #T[20 'D' 'the' 93 the_d1] #T[30 'N' 'man' 39 man_n1]] #T[40 'V' 'runs' 39 runs_v1]]"
+        # Real text
+        self.ace_output = """ tree 1 #T[1 "S" nil 843 sb-hd_mc_c #T[2 "NP" nil 837 hdn_bnp-qnt_c #T[3 "NP" "I" 85 i]] #T[4 "VP" nil 842 hd-cmp_u_c #T[5 "V" nil 838 v_n3s-bse_ilr #T[6 "V" "like" 56 like_v1]] #T[7 "NP" nil 841 hdn_bnp_c #T[8 "N" nil 840 w_period_plr #T[9 "N" nil 839 n_pl_olr #T[10 "N" "dogs." 62 dog_n1]]]]]] "I like dogs.\""""
+
+    def test_nested_text(self):
+        tree = Derivation(self.nested_text)
+        self.assertTrue(tree.edge_ID == "1")
+        self.assertTrue(tree.label == "S")
+        self.assertTrue(tree.token == None)
+        self.assertTrue(tree.chart_ID == "3")
+        self.assertTrue(tree.rule_name == "subj-head")
+        self.assertTrue(len(tree.children) == 2)
+        # Child 1
+        child = tree.children[0]
+        self.assertTrue(child.edge_ID == "2")
+        self.assertTrue(child.label == "NP")
+        self.assertTrue(child.token == None)
+        self.assertTrue(child.chart_ID == "93")
+        self.assertTrue(child.rule_name == "spec-head")
+        self.assertTrue(len(child.children) == 2)
+        # Child 1a
+        child = child.children[0]
+        self.assertTrue(child.edge_ID == "2")
+        self.assertTrue(child.label == "D")
+        self.assertTrue(child.token == "the")
+        self.assertTrue(child.chart_ID == "93")
+        self.assertTrue(child.rule_name == "the_d1")
+        self.assertTrue(len(child.children) == 0)
+        # Child 1b
+        child = tree.children[0].children[1]
+        self.assertTrue(child.edge_ID == "3")
+        self.assertTrue(child.label == "N")
+        self.assertTrue(child.token == "man")
+        self.assertTrue(child.chart_ID == "39")
+        self.assertTrue(child.rule_name == "man_n1")
+        self.assertTrue(len(child.children) == 0)
+        # Child 2
+        child = tree.children[1]
+        self.assertTrue(child.edge_ID == "3")
+        self.assertTrue(child.label == "V")
+        self.assertTrue(child.token == "runs")
+        self.assertTrue(child.chart_ID == "39")
+        self.assertTrue(child.rule_name == "runs_v1")
+        self.assertTrue(len(child.children) == 0)
+
+    def test_high_count(self):
+        tree = Derivation(self.nested_high_count)
+        self.assertTrue(tree.edge_ID == "10")
+        self.assertTrue(tree.label == "S")
+        self.assertTrue(tree.token == None)
+        self.assertTrue(tree.chart_ID == "3")
+        self.assertTrue(tree.rule_name == "subj-head")
+        self.assertTrue(len(tree.children) == 2)
+        # Child 1
+        child = tree.children[0]
+        self.assertTrue(child.edge_ID == "20")
+        self.assertTrue(child.label == "NP")
+        self.assertTrue(child.token == None)
+        self.assertTrue(child.chart_ID == "93")
+        self.assertTrue(child.rule_name == "spec-head")
+        self.assertTrue(len(child.children) == 2)
+        # Child 1a
+        child = child.children[0]
+        self.assertTrue(child.edge_ID == "20")
+        self.assertTrue(child.label == "D")
+        self.assertTrue(child.token == "the")
+        self.assertTrue(child.chart_ID == "93")
+        self.assertTrue(child.rule_name == "the_d1")
+        self.assertTrue(len(child.children) == 0)
+        # Child 1b
+        child = tree.children[0].children[1]
+        self.assertTrue(child.edge_ID == "30")
+        self.assertTrue(child.label == "N")
+        self.assertTrue(child.token == "man")
+        self.assertTrue(child.chart_ID == "39")
+        self.assertTrue(child.rule_name == "man_n1")
+        self.assertTrue(len(child.children) == 0)
+        # Child 2
+        child = tree.children[1]
+        self.assertTrue(child.edge_ID == "40")
+        self.assertTrue(child.label == "V")
+        self.assertTrue(child.token == "runs")
+        self.assertTrue(child.chart_ID == "39")
+        self.assertTrue(child.rule_name == "runs_v1")
+        self.assertTrue(len(child.children) == 0)
+
+    def test_read_ACE_output(self):
+        tree = Derivation(self.ace_output_text)
+        self.assertTrue(tree.tree_ID == "1")
+        self.assertTrue(tree.edge_ID == "1")
+        self.assertTrue(tree.label == "LABEL")
+        self.assertTrue(tree.token == "TOKEN")
+        self.assertTrue(tree.chart_ID == "2")
+        self.assertTrue(tree.rule_name == "RULE_NAME")
+        self.assertTrue(len(tree.children) == 0)
+
+    def test_read_ACE_output2(self):
+        tree = Derivation(self.ace_output_text2)
+        self.assertTrue(tree.tree_ID == "1")
+        self.assertTrue(tree.edge_ID == "1")
+        self.assertTrue(tree.label == "LABEL")
+        self.assertTrue(tree.token == "TOKEN")
+        self.assertTrue(tree.chart_ID == "2")
+        self.assertTrue(tree.rule_name == "RULE_NAME")
 
     def test_read_ACE_terminal(self):
         tree = Derivation(self.terminal_text)
@@ -120,18 +229,99 @@ class derivationtree_test(unittest.TestCase):
         self.assertTrue(tree.children[2].rule_name == "RULE_NAME4")
         self.assertTrue(len(tree.children[2].children) == 0)
 
+    def test_read_ACE_real(self):
+        tree = Derivation(self.ace_output)
+        self.assertTrue(tree.edge_ID == "1")
+        self.assertTrue(tree.label == "S")
+        self.assertTrue(tree.token == None)
+        self.assertTrue(tree.chart_ID == "843")
+        self.assertTrue(tree.rule_name == "sb-hd_mc_c")
+        self.assertEqual(len(tree.children), 2)
+        # Child 1
+        child1 = tree.children[0]
+        self.assertTrue(child1.edge_ID == "2")
+        self.assertTrue(child1.label == "NP")
+        self.assertTrue(child1.token == None)
+        self.assertTrue(child1.chart_ID == "837")
+        self.assertTrue(child1.rule_name == "hdn_bnp-qnt_c")
+        self.assertEqual(len(child1.children), 1)
+        # Child 1.a
+        child1a = child1.children[0]
+        self.assertTrue(child1a.edge_ID == "3")
+        self.assertTrue(child1a.label == "NP")
+        self.assertTrue(child1a.token == "I")
+        self.assertTrue(child1a.chart_ID == "85")
+        self.assertTrue(child1a.rule_name == "i")
+        self.assertEqual(len(child1a.children), 0)
+        # Child 2
+        child2 = tree.children[1]
+        self.assertTrue(child2.edge_ID == "4")
+        self.assertTrue(child2.label == "VP")
+        self.assertTrue(child2.token == None)
+        self.assertTrue(child2.chart_ID == "842")
+        self.assertTrue(child2.rule_name == "hd-cmp_u_c")
+        self.assertEqual(len(child2.children), 2)
+        # Child 2.a
+        child2a = child2.children[0]
+        self.assertTrue(child2a.edge_ID == "5")
+        self.assertTrue(child2a.label == "V")
+        self.assertTrue(child2a.token == None)
+        self.assertTrue(child2a.chart_ID == "838")
+        self.assertTrue(child2a.rule_name == "v_n3s-bse_ilr")
+        self.assertEqual(len(child2a.children), 1)
+        # Child 2.a.1
+        child2a1 = child2a.children[0]
+        self.assertTrue(child2a1.edge_ID == "6")
+        self.assertTrue(child2a1.label == "V")
+        self.assertTrue(child2a1.token == "like")
+        self.assertTrue(child2a1.chart_ID == "56")
+        self.assertTrue(child2a1.rule_name == "like_v1")
+        self.assertEqual(len(child2a1.children), 0)
+        # Child 2.b
+        child2b = child2.children[1]
+        self.assertTrue(child2b.edge_ID == "7")
+        self.assertTrue(child2b.label == "NP")
+        self.assertTrue(child2b.token == None)
+        self.assertTrue(child2b.chart_ID == "841")
+        self.assertTrue(child2b.rule_name == "hdn_bnp_c")
+        self.assertEqual(len(child2b.children), 1)
+        # Child 2.b.1
+        child2b1 = child2b.children[0]
+        self.assertTrue(child2b1.edge_ID == "8")
+        self.assertTrue(child2b1.label == "N")
+        self.assertTrue(child2b1.token == None)
+        self.assertTrue(child2b1.chart_ID == "840")
+        self.assertTrue(child2b1.rule_name == "w_period_plr")
+        self.assertEqual(len(child2b1.children), 1)
+        # Child 2.b.1.a
+        child2b1a = child2b1.children[0]
+        self.assertTrue(child2b1a.edge_ID == "9")
+        self.assertTrue(child2b1a.label == "N")
+        self.assertTrue(child2b1a.token == None)
+        self.assertTrue(child2b1a.chart_ID == "839")
+        self.assertTrue(child2b1a.rule_name == "n_pl_olr")
+        self.assertEqual(len(child2b1a.children), 1)
+        # Child 2.b.1.a.1
+        child2b1a1 = child2b1a.children[0]
+        self.assertTrue(child2b1a1.edge_ID == "10")
+        self.assertTrue(child2b1a1.label == "N")
+        self.assertTrue(child2b1a1.token == "dogs.")
+        self.assertTrue(child2b1a1.chart_ID == "62")
+        self.assertTrue(child2b1a1.rule_name == "dog_n1")
+        self.assertEqual(len(child2b1a1.children), 0)
+
     ## Test output methods
-    def test_get_HTML_terminal(self):
+    def test_output_HTML_terminal(self):
         expected = "<div id=2 title=\"2: RULE_NAME\"><p>LABEL</p><p>TOKEN</p></div>"
-        self.assertEqual(Derivation(self.terminal_text).get_HTML(), expected)
+        self.assertEqual(Derivation(self.terminal_text).output_HTML(), expected)
 
-    def test_get_HTML_unary(self):
+    def test_output_HTML_unary(self):
         expected = "<div id=93 title=\"93: RULE_NAME\"><p>LABEL</p><div id=39 title=\"39: RULE_NAME2\"><p>LABEL2</p><p>TOKEN</p></div></div>"
-        self.assertEqual(Derivation(self.unary_text).get_HTML(), expected)
+        self.assertEqual(Derivation(self.unary_text).output_HTML(), expected)
 
-    def test_get_HTML_binary(self):
-        expected = "<div id=93 title=\"93: RULE_NAME\"><p>LABEL</p><div id=39 title=\"39: RULE_NAME2\"><p>LABEL2</p><p>TOKEN</p></div> <div id=240 title=\"240: RULE_NAME3\"><p>LABEL3</p><p>TOKEN2</p></div></div>"
-        self.assertEqual(Derivation(self.binary_text).get_HTML(), expected)
+    def test_output_HTML_binary(self):
+        expected = "<div id=93 title=\"93: RULE_NAME\"><p>LABEL</p><div id=39 title=\"39: RULE_NAME2\"><p>LABEL2</p><p>TOKEN</p></div><div id=240 title=\"240: RULE_NAME3\"><p>LABEL3</p><p>TOKEN2</p></div></div>"
+        self.assertEqual(Derivation(self.binary_text).output_HTML(), expected)
 
     ## Test equals methods
     # Terminal
