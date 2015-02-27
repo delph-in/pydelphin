09486488a5c4b4e913afa18aa8150084e912095f
T.J. Trimble <trimblet@me.com>
2015-02-27 12:40:36 -0800

    Working derivation tree class with HTML output.
    NOTE: modified by @goodmami (Michael Wayne Goodman) to not include ACE files.

---
+++ delphin/avm/avm.py
@@ -0,0 +1,16 @@
+
+
+class AVM(object):
+    
+    def __init__(self, definition):
+        # For now, assume ACE input
+        pass
+
+
+    @class_method
+    def read_ACE(self):
+        """
+        Format:
+            
+        """
+        pass
--- delphin/derivations/derivationtree.py
+++ delphin/derivations/derivationtree.py
@@ -7,20 +7,20 @@ class Derivation(object):
     @author: T.J. Trimble
     """
 
+    legal_punctuation = re.escape("-+=/:.!?$<>@#%^&*")
     quotes = re.compile("\'|\"")
-
     ace_pattern = " ".join(("(?P<EDGE_ID>\d+)",
                             "(?P<LABEL>{0}[\w{1}]+{0})",
                             "(?P<TOKEN>({0}[\w{1}]+{0}|nil))",
                             "(?P<CHART_ID>\d+)",
-                            "(?P<RULE_NAME>[\w{1}]+)",)).format("[\'\"]?", re.escape("-+=/:.!?$<>@#%^&*"))
+                            "(?P<RULE_NAME>[\w{1}]+)",)).format("[\'\"]?", legal_punctuation)
 
     def __init__(self, definition, data_package=None, children=None):
         """
         Two constructors:
             Derivation(str:definition):
-                Construct a Derivation object from a bracketed tree structure.
-                ex. Derivation("(S (NP (N I)) (VP (V fell)))")
+                Construct a Derivation object from a bracketed tree structure from ACE.
+                ex. Derivation("#T[<EDGE_ID> "<LABEL>" "<TOKEN>"|nil >CHART_ID> <RULE_NAME> <CHILDREN>]")
 
             Derivation(None, tuple:data_package, list:children):
                 Construct a Derivation object from tuple of values and a list of children.
@@ -159,9 +159,9 @@ class Derivation(object):
         Two trees are equal if their labels, tokens, rule names, and 
         structures are the same. Edge IDs and chart IDs are irrelevant.
         """
-        # Check attributes
         if not isinstance(other, Derivation):
             return False
+        # Check attributes
         if self.label != other.label:
             return False
         if self.token != other.token:
@@ -206,27 +206,46 @@ class Derivation(object):
         return self._output(format_string="#T[{EDGE_ID}{LABEL}{TOKEN}{CHART_ID}{RULE}{CHILDREN}]")
 
     # HTML Methods
-    def output_HTML(self, title_text=True):
+    def output_HTML(self, top=True, title_text=True):
         """
         Returns HTML representation of tree in the following format:
-            <div id=CHART_ID title="CHART_ID: RULE_NAME"><p>LABEL</p>(<p>TOKEN</p>)?</div>
+            
+            <div>
+                <ul>
+                    <li>
+                        <a id={CHART_ID}{TITLE}>{PARENT_LABEL}</a>
+                        <ul>
+                            <li><a id={CHART_ID}{TITLE}><p>{TERMINAL_LABEL}</p><p>{TOKEN}</p></a></li>
+                            <li><a id={CHART_ID}{TITLE}><p>{TERMINAL_LABEL}</p><p>{TOKEN}</p></a></li>
+                        </ul>
+                    </li>
+                    <li><a id={CHART_ID}{TITLE}>{TERMINAL_LABEL}</a></li>
+                </ul>
+            </div>
+
+        See http://thecodeplayer.com/walkthrough/css3-family-tree
         
         By default, this method returns HTML styled with the HTML's title
         attribute set to the rule used and the parse chart ID. Pass
         title_text=False to disable this.
         """
-        result = "<div id={CHART_ID}{TITLE}><p>{LABEL}</p>{TOKEN}{CHILDREN}</div>"
+        top_formatter = "<div class=\"derivationTree\"><ul>{}</ul></div>"
+        formatter = "<li{CLASS}{CHART_ID}{TITLE}><p>{LABEL}</p>{TOKEN}{CHILDREN}</li>"
         # Add token if applicable
         values = {
-            "CHART_ID": self.chart_ID,
-            "RULE_NAME": self.rule_name,
+            "CLASS": " class=\"terminal\"" if not self.children else "",
+            "CHART_ID": " id={}".format(self.chart_ID),
+            "TITLE": " title=\"{}: {}\"".format(self.chart_ID, self.rule_name) if title_text else "",
             "LABEL": self.label,
             "TOKEN": "<p>{}</p>".format(self.token) if self.token else "",
-            "TITLE": " title=\"{}: {}\"".format(self.chart_ID, self.rule_name) if title_text else "",
-            "CHILDREN": "".join(child.output_HTML(title_text=title_text) for child in self.children)
+            "CHILDREN": "<ul>{}</ul>".format("".join(child.output_HTML(top=False, title_text=title_text) for child in self.children)) if self.children else ""
         }
         # Return result
-        return result.format(**values)
+        result = formatter.format(**values)
+        if top:
+            result = top_formatter.format(result)
+        return result
+        #result = "<div id={CHART_ID}{TITLE}><p>{LABEL}</p>{TOKEN}{CHILDREN}</div>"
 
     # Pickle Methods
     def read(self):
