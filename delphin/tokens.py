29e78875b43972821f811e815f8e6a71ccff0d97
Michael Wayne Goodman <goodman.m.w@gmail.com>
2016-07-11 14:48:42 -0700

    Add tokens module and associated tests.

---
+++ delphin/tokens.py
@@ -0,0 +1,174 @@
+
+import re
+from collections import namedtuple
+
+from delphin.mrs.components import Lnk
+
+_yy_token = namedtuple(
+    'YyToken',
+    (
+        'id',       # token identifier
+        'start',    # start vertex
+        'end',      # end vertex
+        'lnk',      # <from:to> charspan (optional)
+        'paths',    # path membership
+        'form',     # surface token
+        'surface',  # original token (optional; only if `form` was modified)
+        'ipos',     # length of `lrules`? always 0?
+        'lrules',   # something about lexical rules; always "null"?
+        'pos'       # pairs of (POS, prob)
+    )
+)
+
+class YyToken(_yy_token):
+    """
+    A tuple of token data in the YY format.
+
+    Args:
+        id: token identifier
+        start: start vertex
+        end: end vertex
+        lnk: <from:to> charspan (optional)
+        paths: path membership
+        form: surface token
+        surface: original token (optional; only if `form` was modified)
+        ipos: length of `lrules`? always 0?
+        lrules: something about lexical rules; always "null"?
+        pos: pairs of (POS, prob)
+    """
+    def __new__(cls, id, start, end,
+                lnk=None, paths=(), form=None, surface=None,
+                ipos=0, lrules=("null",), pos=()):
+        if form is None:
+            raise TypeError('Missing required keyword argument \'form\'.')
+        return super(YyToken, cls).__new__(
+            cls, id, start, end, lnk, list(paths), form, surface,
+            ipos, list(lrules), list(pos)
+        )
+
+    def __str__(self):
+        parts = [str(self.id), str(self.start), str(self.end)]
+        if self.lnk is not None:
+            parts.append(str(self.lnk))
+        parts.append(' '.join(map(str, self.paths or [1])))
+        if self.surface is None:
+            parts.append('"{}"'.format(self.form))
+        else:
+            parts.append('"{}" "{}"'.format(self.form, self.surface))
+        parts.extend([
+            str(self.ipos),
+            ' '.join(map('"{}"'.format, self.lrules))
+        ])
+        if self.pos:
+            ps = ['"{}" {:.4f}'.format(pos, p) for pos, p in self.pos]
+            parts.append(' '.join(ps))
+        return '({})'.format(', '.join(parts))
+
+    @classmethod
+    def from_dict(cls, d):
+        """
+        Decode from a dictionary as from YyToken.to_dict().
+        """
+        return cls(
+            d['id'],
+            d['start'],
+            d['end'],
+            Lnk.charspan(d['from'], d['to']) if 'from' in d else None,
+            d.get('paths'),
+            d['form'],
+            d.get('surface'),
+            # ipos=
+            # lrules=
+            pos=zip(d.get('tags', []), d.get('probabilities', []))
+        )
+
+    def to_dict(self):
+        """
+        Encode the token as a dictionary suitable for JSON serialization.
+        """
+        d = {
+            'id': self.id,
+            'start': self.start,
+            'end': self.end,
+            'form': self.form
+        }
+        if self.lnk is not None:
+            cfrom, cto = self.lnk.data
+            d['from'] = cfrom
+            d['to'] = cto
+        if self.surface is not None:
+            d['surface'] = self.surface
+        # if self.ipos != 0:
+        #     d['ipos'] = self.ipos
+        #     d['lrules'] = self.lrules
+        if self.pos:
+            d['tags'] = [ps[0] for ps in self.pos]
+            d['probabilities'] = [ps[1] for ps in self.pos]
+        return d
+
+
+# from: http://moin.delph-in.net/PetInput
+# (id, start, end, [link,] path+, form [surface], ipos, lrule+[, {pos p}+])
+_yy_re = re.compile(
+    r'\(\s*'
+    r'(?P<id>{integer}){comma}'
+    r'(?P<start>{integer}){comma}'
+    r'(?P<end>{integer}){comma}'
+    r'(?:<(?P<lnkfrom>{integer}):(?P<lnkto>{integer})>{comma})?'
+    r'(?P<paths>(?:{integer}\s*)+){comma}'
+    r'(?P<form>{string})'
+    r'(?:\s*(?P<surface>{string}))?'
+    r'{comma}'
+    r'(?P<ipos>{integer}){comma}'
+    r'(?P<lrules>(?:{string}\s*)+)'
+    r'(?:{comma}(?P<pos>(?:{string}\s+{float}\s*)+))?'
+    r'\s*\)'
+    .format(
+        integer=r'-?\d+',
+        comma=r'\s*,\s*',
+        string=r'"[^"\\]*(?:\\.[^"\\]*)*"',
+        float=r'-?(0|[1-9]\d*)(\.\d+[eE][-+]?|\.|[eE][-+]?)\d+'
+    )
+)
+
+class YyTokenLattice(object):
+    """
+    A lattice of YY Tokens.
+
+    Args:
+        tokens: a list of YyToken objects
+    """
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    @classmethod
+    def from_string(cls, s):
+        def _qstrip(s):
+            return s[1:-1]  # remove assumed quote characters
+        tokens = []
+        for match in _yy_re.finditer(s):
+            d = match.groupdict()
+            lnk, pos = None, []
+            if d['lnkfrom'] is not None:
+                lnk = Lnk.charspan(d['lnkfrom'], d['lnkto'])
+            if d['pos'] is not None:
+                ps = d['pos'].strip().split()
+                pos = list(zip(map(_qstrip, ps[::2]), map(float, ps[1::2])))
+            tokens.append(
+                YyToken(
+                    int(d['id']),
+                    int(d['start']),
+                    int(d['end']),
+                    lnk,
+                    list(map(int, d['paths'].strip().split())),
+                    _qstrip(d['form']),
+                    None if d['surface'] is None else _qstrip(d['surface']),
+                    int(d['ipos']),
+                    list(map(_qstrip, d['lrules'].strip().split())),
+                    pos
+                )
+            )
+        return cls(tokens)
+
+    def __str__(self):
+        return ' '.join(map(str, self.tokens))
---
+++ tests/tokens_test.py
@@ -0,0 +1,156 @@
+
+import pytest
+
+from delphin.tokens import YyTokenLattice as YY, YyToken
+from delphin.mrs.components import Lnk
+
+token_v1_basic = '(1, 0, 1, 1, "dog", 0, "null")'
+token_v1_surface = '(1, 0, 1, 1, "dog" "Dog", 0, "null")'
+token_v1_pos = '(1, 0, 1, 1, "dog", 0, "null", "NN" 0.8 "VV" 0.2000)'
+token_v1_surface_pos = '(1, 0, 1, 1, "dog" "Dog", 0, "null", "NN" 1.0000)'
+token_v1_lrules = '(1, 0, 1, 1, "dog", 2, "lrule1" "lrule2")'
+token_v2 = '(1, 0, 1, <1:3>, 1, "dog" "Dog", 0, "null", "NN" 1.0000)'
+
+tokenstring = (
+  '(42, 0, 1, <0:12>, 1, "Tokenization", 0, "null", "NNP" 0.7677 "NN" 0.2323) '
+  '(43, 1, 2, <12:13>, 1, ",", 0, "null", "," 1.0000) '
+  '(44, 2, 3, <14:15>, 1, "a", 0, "null", "DT" 1.0000) '
+  '(45, 3, 4, <16:27>, 1, "non-trivial", 0, "null", "JJ" 1.0000) '
+  '(46, 4, 5, <28:36>, 1, "exercise", 0, "null", "NN" 0.9887 "VB" 0.0113) '
+  '(47, 5, 6, <36:37>, 1, ",", 0, "null", "," 1.0000) '
+  '(48, 6, 7, <38:44>, 1, "bazed", 0, "null", "VBD" 0.5975 "VBN" 0.4025) '
+  '(49, 7, 8, <45:58>, 1, "oe@ifi.uio.no", 0, "null", "NN" 0.7342 "JJ" 0.2096) '
+  '(50, 8, 9, <58:59>, 1, ".", 0, "null", "." 1.0000)'
+)
+
+def check_token(t, id, start, end, lnk, paths, form, surf, ipos, lrules, pos):
+    assert t.id == id
+    assert t.start == start
+    assert t.end == end
+    assert t.lnk == lnk
+    assert t.paths == paths
+    assert t.form == form
+    assert t.surface == surf
+    assert t.ipos == ipos
+    assert t.lrules == lrules
+    assert t.pos == pos
+
+
+class YYToken(object):
+    def test_init(self):
+        with pytest.raises(TypeError):
+            YyToken()
+            YyToken(1)
+            YyToken(1, 0)
+            YyToken(1, 0, 1)
+            YyToken(1, 0, 1, Lnk.charspan(0,1))
+            YyToken(1, 0, 1, Lnk.charspan(0,1), [1])
+            YyToken(1, 0, 1, Lnk.charspan(0,1), [1], surface=".")
+            YyToken(1, 0, 1, Lnk.charspan(0,1), [1], surface=".", ipos=0)
+            YyToken(1, 0, 1, Lnk.charspan(0,1), [1], surface=".",
+                    ipos=0, lrules=["null"])
+            YyToken(1, 0, 1, Lnk.charspan(0,1), [1], surface=".",
+                    ipos=0, lrules=["null"], pos=[(".", 1.0)])
+        t = YyToken(1, 0, 1, form="dog")
+        check_token(t, 1, 0, 1, None, [], "dog", None, 0, ["null"], [])
+        t = YyToken(1, 0, 1, Lnk.charspan(0,1), [1], "dog", "Dog",
+                    ipos=0, lrules=["null"], pos=[("NN", 1.0)])
+        check_token(t, 1, 0, 1, Lnk.charspan(0,1), [1], "dog", "Dog",
+                    0, ["null"], [("NN", 1.0)])
+
+    def from_dict(self):
+        t = YyToken.from_dict({'id':1, 'start': 0, 'end': 1, 'form': "dog"})
+        check_token(t, 1, 0, 1, None, [], "dog", None, 0, ["null"], [])
+        t = YyToken.from_dict({
+            'id': 1, 'start': 0, 'end': 1, 'lnk':Lnk.charspan(0,1),
+            'paths': [1], 'form': "dog", 'surface': "Dog",
+            #'ipos': 0, 'lrules': ["null"],
+            'tags': ["NN"], 'probabilities': [1.0]
+        })
+        check_token(t, 1, 0, 1, Lnk.charspan(0,1), [1], "dog", "Dog",
+                    0, ["null"], [("NN", 1.0)])
+
+    def to_dict(self):
+        t = YyToken(1, 0, 1, form="dog")
+        assert t.to_dict() == {'id':1, 'start': 0, 'end': 1, 'form': "dog"}
+        t = YyToken(1, 0, 1, Lnk.charspan(0,1), [1], "dog", "Dog",
+                    ipos=0, lrules=["null"], pos=[("NN", 1.0)])
+        assert t.to_dict() == {
+            'id': 1, 'start': 0, 'end': 1, 'lnk':Lnk.charspan(0,1),
+            'paths': [1], 'form': "dog", 'surface': "Dog",
+            #'ipos': 0, 'lrules': ["null"],
+            'tags': ["NN"], 'probabilities': [1.0]
+        }
+
+class TestYYTokenLattice(object):
+
+    def test_fromstring(self):
+        assert len(YY.from_string(token_v1_basic).tokens) == 1
+        t = YY.from_string(token_v1_basic).tokens[0]
+        check_token(t, 1, 0, 1, None, [1], "dog", None, 0, ["null"], [])
+        t = YY.from_string(token_v1_surface).tokens[0]
+        check_token(t, 1, 0, 1, None, [1], "dog", "Dog", 0, ["null"], [])
+        t = YY.from_string(token_v1_pos).tokens[0]
+        check_token(t, 1, 0, 1, None, [1], "dog", None, 0, ["null"],
+                    [("NN", 0.8), ("VV", 0.2)])
+        t = YY.from_string(token_v1_surface_pos).tokens[0]
+        check_token(t, 1, 0, 1, None, [1], "dog", "Dog", 0, ["null"],
+                    [("NN", 1.0)])
+        t = YY.from_string(token_v1_lrules).tokens[0]
+        check_token(t, 1, 0, 1, None, [1], "dog", None, 2,
+                    ["lrule1", "lrule2"], [])
+        t = YY.from_string(token_v2).tokens[0]
+        check_token(t, 1, 0, 1, Lnk.charspan(1,3), [1], "dog", "Dog",
+                    0, ["null"], [("NN", 1.0)])
+        tl = YY.from_string(tokenstring)
+        assert len(tl.tokens) == 9
+        check_token(
+            tl.tokens[0],
+            42, 0, 1, Lnk.charspan(0,12), [1], "Tokenization", None,
+            0, ["null"], [("NNP", 0.7677), ("NN", 0.2323)]
+        )
+        check_token(
+            tl.tokens[1],
+            43, 1, 2, Lnk.charspan(12,13), [1], ",", None,
+            0, ["null"], [(",", 1.0000)]
+        )
+        check_token(
+            tl.tokens[2],
+            44, 2, 3, Lnk.charspan(14,15), [1], "a", None,
+            0, ["null"], [("DT", 1.0000)]
+        )
+        check_token(
+            tl.tokens[3],
+            45, 3, 4, Lnk.charspan(16,27), [1], "non-trivial", None,
+            0, ["null"], [("JJ", 1.0000)]
+        )
+        check_token(
+            tl.tokens[4],
+            46, 4, 5, Lnk.charspan(28,36), [1], "exercise", None,
+            0, ["null"], [("NN", 0.9887), ("VB", 0.0113)]
+        )
+        check_token(
+            tl.tokens[5],
+            47, 5, 6, Lnk.charspan(36,37), [1], ",", None,
+            0, ["null"], [(",", 1.0000)]
+        )
+        check_token(
+            tl.tokens[6],
+            48, 6, 7, Lnk.charspan(38,44), [1], "bazed", None,
+            0, ["null"], [("VBD", 0.5975), ("VBN", 0.4025)]
+        )
+        check_token(
+            tl.tokens[7],
+            49, 7, 8, Lnk.charspan(45,58), [1], "oe@ifi.uio.no", None,
+            0, ["null"], [("NN", 0.7342), ("JJ", 0.2096)]
+        )
+        check_token(
+            tl.tokens[8],
+            50, 8, 9, Lnk.charspan(58,59), [1], ".", None,
+            0, ["null"], [(".", 1.0000)]
+        )
+
+    def test_str(self):
+        assert str(YY.from_string(token_v1_basic).tokens[0]) == token_v1_basic
+        assert str(YY.from_string(token_v2).tokens[0]) == token_v2
+        assert str(YY.from_string(tokenstring)) == tokenstring
