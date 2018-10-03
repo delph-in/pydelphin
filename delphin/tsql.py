1434137ca8cbfd451d29a756db0e1f92e14794e1
Michael Wayne Goodman <goodman.m.w@gmail.com>
2018-10-02 21:07:00 -0700

    Improve TSQL support for conditions

--- delphin/tsql.py
+++ delphin/tsql.py
@@ -1,10 +1,12 @@
 
-from datetime import datetime
 import re
 
 from delphin.exceptions import TSQLSyntaxError
-from delphin.util import LookaheadIterator
+from delphin.util import LookaheadIterator, parse_datetime
+from delphin import itsdb
 
+
+### QUERY PROCESSING ##########################################################
 
 def query(query, ts):
     queryobj = _parse_query(LookaheadIterator(_lex(query)))
@@ -13,7 +15,103 @@ def query(query, ts):
 
 def select(query, ts):
     queryobj = _parse_select(LookaheadIterator(_lex(query)))
-    return queryobj
+    # start with 'from' tables, apply constraints, join projection
+    table = _select_from(queryobj['tables'], None, ts)
+    table = _select_where(queryobj['where'], table, ts)
+    table = _select_projection(queryobj['projection'], table, ts)
+    # finally select the relevant columns from the joined table
+    return itsdb.select_rows(queryobj['projection'], table)
+
+
+def _select_from(tables, table, ts):
+    joined = set([] if table is None else table.name.split('+'))
+    for tab in tables:
+        if tab not in joined:
+            joined.add(tab)
+            table = _transitive_join(table, ts[tab], ts, 'inner')
+    return table
+
+
+def _select_where(condition, table, ts):
+    if condition is not None:
+        func, fields = _process_condition(condition)
+        for field in fields:
+            table = _join_if_missing(table, field, ts, 'left')
+        table = itsdb.Table(
+            table.name,
+            table.fields,
+            list(filter(func, table)))
+    return table
+
+
+def _process_condition(condition):
+    op, body = condition
+    if op == 'and':
+        lfunc, lfields = _process_condition(body[0])
+        rfunc, rfields = _process_condition(body[1])
+        func = lambda row, lfunc=lfunc, rfunc=rfunc: lfunc(row) and rfunc(row)
+        fields = lfields + rfields
+    elif op == 'or':
+        lfunc, lfields = _process_condition(body[0])
+        rfunc, rfields = _process_condition(body[1])
+        func = lambda row, lfunc=lfunc, rfunc=rfunc: lfunc(row) or rfunc(row)
+        fields = lfields + rfields
+    elif op == 'not':
+        nfunc, fields = _process_condition(body)
+        func = lambda row, nfunc=nfunc: not nfunc(row)
+    else:
+        col, val = body
+        fields = [col]
+        if op == '~':
+            func = lambda row, val=val, col=col: re.search(val, row[col])
+        elif op == '!~':
+            func = lambda row, val=val, col=col: not re.search(val, row[col])
+        elif op == '==':
+            func = lambda row, val=val, col=col: row[col] == val
+        elif op == '!=':
+            func = lambda row, val=val, col=col: row[col] != val
+        elif op == '<':
+            func = lambda row, val=val, col=col: row[col] < val
+        elif op == '<=':
+            func = lambda row, val=val, col=col: row[col] <= val
+        elif op == '>':
+            func = lambda row, val=val, col=col: row[col] > val
+        elif op == '>=':
+            func = lambda row, val=val, col=col: row[col] >= val
+    return func, fields
+
+
+def _select_projection(projection, table, ts):
+    for p in projection:
+        table = _join_if_missing(table, p, ts, 'inner')
+    return table
+
+
+def _join_if_missing(table, col, ts, how):
+    tab, _, column = col.rpartition(':')
+    if not tab:
+        # Just get the first table defining the column. This
+        # makes the assumption that relations are ordered and
+        # that the first one is 'primary'
+        tab = ts.relations.find(column)[0]
+    if table is None or column not in table.fields:
+        table = _transitive_join(table, ts[tab], ts, how)
+    return table
+
+
+def _transitive_join(tab1, tab2, ts, how):
+    if tab1 is None:
+        table = tab2
+    else:
+        table = tab1
+        # if the tables aren't directly joinable but are joinable
+        # transitively via a 'path' of table joins, do so first
+        path = _id_path(tab1, tab2, ts.relations)
+        for intervening, _ in path[1:]:
+            table = itsdb.join(table, ts[intervening], how=how)
+        # now the tables are either joinable or no path existed
+        table = itsdb.join(table, tab2, how=how)
+    return table
 
 
 def _id_path(src, tgt, rels):
@@ -23,9 +121,9 @@ def _id_path(src, tgt, rels):
     This is just a basic breadth-first-search. The relations file
     should be small enough to not be a problem.
     """
-    paths = [[(src, key)] for key in rels[src].keys()]
-    tgtkeys = set(rels[tgt].keys())
-    visited = set([src])
+    paths = [[(src.name, key)] for key in src.fields.keys()]
+    tgtkeys = set(tgt.fields.keys())
+    visited = set(src.name.split('+'))
     while True:
         newpaths = []
         for path in paths:
@@ -46,6 +144,8 @@ def _id_path(src, tgt, rels):
         else:
             break
 
+
+### QUERY PARSING #############################################################
 
 _keywords = list(map(re.escape,
                      ('info', 'set', 'retrieve', 'select', 'insert',
@@ -130,7 +230,7 @@ def _parse_select(tokens):
     tables = _parse_select_from(tokens)
     condition = _parse_select_where(tokens)
 
-    if projection == '*' and tables is None and condition is None:
+    if projection == '*' and not tables and condition is None:
         raise TSQLSyntaxError(
             "'select *' requires a 'from' or 'where' statement",
             lineno=lineno, text=token)
@@ -170,14 +270,12 @@ def _prepare_columns(cols):
 
 
 def _parse_select_from(tokens):
+    tables = []
     if tokens.peek()[1] == 'from':
         tokens.next()
-        tables = []
         while tokens.peek()[0] == 10:
             _, table, _ = tokens.next()
             tables.append(table)
-    else:
-        tables = None
     return tables
 
 
@@ -207,7 +305,7 @@ def _parse_condition_disjunction(tokens):
     elif len(conds) == 1:
         return conds[0]
     else:
-        return ('or', conds)
+        return ('or', tuple(conds))
 
 
 def _parse_condition_conjunction(tokens):
@@ -237,7 +335,7 @@ def _parse_condition_conjunction(tokens):
     elif len(conds) == 1:
         return conds[0]
     else:
-        return ('and', conds)
+        return ('and', tuple(conds))
 
 
 def _parse_condition_negation(tokens):
@@ -253,7 +351,7 @@ def _parse_condition_group(tokens):
     cond = _parse_condition_disjunction(tokens)
     gid, token, lineno = tokens.next()
     _expect(gid == 3 and token == ')', token, lineno, "')'")
-    return cond
+    return tuple(cond)
 
 
 def _parse_condition_statement(tokens):
@@ -274,51 +372,10 @@ def _parse_condition_statement(tokens):
             .format(op), lineno=lineno, text=op)
     else:
         if gid in (6, 7, 8):
-            value = _parse_datetime(value, gid)
+            value = parse_datetime(value)
         elif gid == 9:
             value = int(value)
         return (op, (column, value))
-
-
-def _parse_datetime(s, gid):
-    month_re = re.compile(r'(jan)|(feb)|(mar)|(apr)|(may)|(jun)|'
-                          r'(jul)|(aug)|(sep)|(oct)|(nov)|(dec)',
-                          flags=re.IGNORECASE)
-    if gid == 6:
-        s = re.sub(r'''
-            (?P<y>[0-9]{4})
-            -(?P<m>[0-9]{1,2}|\w{3})
-            (?:-(?P<d>[0-9]{1,2}))?
-            (?:\s*\(?
-                (?P<H>[0-9]{2}):(?P<M>[0-9]{2})(?::(?P<S>[0-9]{2}))?
-            \)?)?''', _date_fix, s, flags=re.VERBOSE)
-        dt = datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
-    elif gid == 7:
-        s = re.sub(r'''
-            (?:(?P<d>[0-9]{1,2})-)?
-            (?P<m>[0-9]{1,2}|\w{3})
-            -(?P<y>[0-9]{2}(?:[0-9]{2})?)
-            (?:\s*\(?
-                (?P<H>[0-9]{2}):(?P<M>[0-9]{2})(?::(?P<S>[0-9]{2}))?
-            \)?)?''', _date_fix, s, flags=re.VERBOSE)
-        dt = datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
-    else:
-        dt = datetime.now()
-    return dt
-
-
-def _date_fix(mo):
-    y = mo.group('y')
-    if len(y) == 2:
-        y = '20' + y  # buggy in ~80yrs or if using ~20yr-old data :)
-    m = mo.group('m')
-    if len(m) == 3:  # assuming 3-letter abbreviations
-        m = str(datetime.strptime(m, '%b').month)
-    d = mo.group('d') or '01'
-    H = mo.group('H') or '00'
-    M = mo.group('M') or '00'
-    S = mo.group('S') or '00'
-    return '{}-{}-{} {}:{}:{}'.format(y, m, d, H, M, S)
 
 
 def _expect(expected, token, lineno, msg):
--- tests/tsql_test.py
+++ tests/tsql_test.py
@@ -4,10 +4,12 @@ from datetime import datetime
 import pytest
 
 from delphin import tsql
+from delphin import itsdb
 from delphin.exceptions import TSQLSyntaxError
 from delphin.util import LookaheadIterator
 
 from .commands_test import mini_testsuite as ts0
+
 
 def test_parse_query():
     parse = lambda s: tsql._parse_query(LookaheadIterator(tsql._lex(s)))
@@ -17,6 +19,7 @@ def test_parse_query():
         parse('set max-results 5')
     with pytest.raises(TSQLSyntaxError):
         parse('insert into item i-id values 10')
+
 
 def test_parse_select():
     parse = lambda s: tsql._parse_select(LookaheadIterator(tsql._lex(s)))
@@ -28,13 +31,13 @@ def test_parse_select():
     assert parse('i-input') == {
         'querytype': 'select',
         'projection': ['i-input'],
-        'tables': None,
+        'tables': [],
         'where': None}
 
     assert parse('i-input i-wf') == {
         'querytype': 'select',
         'projection': ['i-input', 'i-wf'],
-        'tables': None,
+        'tables': [],
         'where': None}
 
     assert parse('i-input i-wf from item') == {
@@ -55,25 +58,25 @@ def test_parse_select_complex_identifiers():
     assert parse('item:i-input') == {
         'querytype': 'select',
         'projection': ['item:i-input'],
-        'tables': None,
+        'tables': [],
         'where': None}
 
     assert parse('item:i-id@i-input') == {
         'querytype': 'select',
         'projection': ['item:i-id', 'item:i-input'],
-        'tables': None,
+        'tables': [],
         'where': None}
 
     assert parse('item:i-id@result:mrs') == {
         'querytype': 'select',
         'projection': ['item:i-id', 'result:mrs'],
-        'tables': None,
+        'tables': [],
         'where': None}
 
     assert parse('item:i-id@i-input mrs') == {
         'querytype': 'select',
         'projection': ['item:i-id', 'item:i-input', 'mrs'],
-        'tables': None,
+        'tables': [],
         'where': None}
 
 
@@ -82,7 +85,7 @@ def test_parse_select_where():
     assert parse('i-input where i-wf = 2') == {
         'querytype': 'select',
         'projection': ['i-input'],
-        'tables': None,
+        'tables': [],
         'where': ('==', ('i-wf', 2))}
 
     assert parse('i-input where i-date < 2018-01-15')['where'] == (
@@ -99,17 +102,62 @@ def test_parse_select_where():
 
     assert parse('i-input '
                  'where i-wf = 2 & i-input ~ \'[Dd]og\'')['where'] == (
-        'and', [('==', ('i-wf', 2)),
-                ('~', ('i-input', '[Dd]og'))])
+        'and', (('==', ('i-wf', 2)),
+                ('~', ('i-input', '[Dd]og'))))
 
     assert parse('i-input '
                  'where i-id = 10 | i-id = 20 & i-wf = 2')['where'] == (
-        'or', [('==', ('i-id', 10)),
-               ('and', [('==', ('i-id', 20)),
-                        ('==', ('i-wf', 2))])])
+        'or', (('==', ('i-id', 10)),
+               ('and', (('==', ('i-id', 20)),
+                        ('==', ('i-wf', 2))))))
 
     assert parse('i-input '
                  'where (i-id = 10 | i-id = 20) & !i-wf = 2')['where'] == (
-        'and', [('or', [('==', ('i-id', 10)),
-                        ('==', ('i-id', 20))]),
-                ('not', ('==', ('i-wf', 2)))])
+        'and', (('or', (('==', ('i-id', 10)),
+                        ('==', ('i-id', 20)))),
+                ('not', ('==', ('i-wf', 2)))))
+
+
+def test_id_path(ts0):
+    ts = itsdb.TestSuite(str(ts0))
+    assert tsql._id_path(ts['item'], ts['item'], ts.relations) == [
+        ('item', 'i-id')]
+    assert tsql._id_path(ts['item'], ts['parse'], ts.relations) == [
+        ('item', 'i-id')]
+    assert tsql._id_path(ts['item'], ts['result'], ts.relations) == [
+        ('item', 'i-id'),
+        ('parse', 'parse-id')]
+    j = itsdb.join(ts['item'], ts['parse'])
+    assert tsql._id_path(j, ts['result'], ts.relations) == [
+        ('item+parse', 'parse-id')]
+
+
+def test_select(ts0):
+    ts = itsdb.TestSuite(str(ts0))
+    assert list(tsql.select('i-input', ts)) == [
+        ['It rained.'], ['Rained.'], ['It snowed.']]
+    assert list(tsql.select('i-input from item', ts)) == [
+        ['It rained.'], ['Rained.'], ['It snowed.']]
+    assert list(tsql.select('i-input from item item', ts)) == [
+        ['It rained.'], ['Rained.'], ['It snowed.']]
+    assert list(tsql.select('i-input from result', ts)) == [
+        ['It rained.'], ['It snowed.']]
+    assert list(tsql.select('i-input from item result', ts)) == [
+        ['It rained.'], ['It snowed.']]
+    assert list(tsql.select('i-id i-input', ts)) == [
+        [10, 'It rained.'], [20, 'Rained.'], [30, 'It snowed.']]
+    res = ts['result']
+    assert list(tsql.select('i-id mrs', ts)) == [
+        [10, res[0]['mrs']], [30, res[1]['mrs']]]
+
+
+def test_select_where(ts0):
+    ts = itsdb.TestSuite(str(ts0))
+    assert list(tsql.select('i-input where i-input ~ "It"', ts)) == [
+        ['It rained.'], ['It snowed.']]
+    assert list(tsql.select('i-input where i-input ~ "It" or i-id = 20', ts)) == [
+        ['It rained.'], ['Rained.'], ['It snowed.']]
+    assert list(tsql.select('i-input where i-date >= 2018-02-01', ts)) == [
+        ['It rained.'], ['Rained.'], ['It snowed.']]
+    assert list(tsql.select('i-input where readings > 0', ts)) == [
+        ['It rained.'], ['It snowed.']]
