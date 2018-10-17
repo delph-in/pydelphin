1e391825d0211603aa7fc93d44f1c57057bad79b
Michael Wayne Goodman <goodman.m.w@gmail.com>
2018-10-16 17:10:58 -0700

    Exclude '|' from TDL identifiers

--- delphin/tdl.py
+++ delphin/tdl.py
@@ -1021,7 +1021,7 @@ _tdl_lex_re = re.compile(
     |%(prefix|suffix)                #  21  start of affixing pattern
     |\(([^ ]+\s+(?:[^ )\\]|\\.)+)\)  #  22  affix subpattern
     |(\/)                            #  23  defaults (currently unused)
-    |([^\s!"#$&'(),./:;<=>[\]^]+)    #  24  identifiers and symbols
+    |([^\s!"#$&'(),./:;<=>[\]^|]+)   #  24  identifiers and symbols
     |(:begin)                        #  25  start a :type or :instance block
     |(:end)                          #  26  end a :type or :instance block
     |(:type|:instance)               #  27  environment type
