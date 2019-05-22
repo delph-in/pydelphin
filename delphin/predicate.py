ee54e75287c44487f120f1c73b46ab8641d5834c
Michael Wayne Goodman <goodman.m.w@gmail.com>
2019-05-22 17:29:23 +0800

    Update predicate documentation.
    Resolves #206

--- delphin/predicate.py
+++ delphin/predicate.py
@@ -1,30 +1,36 @@
 
 """
-Semantic predicates are generally-atomic symbols representing
-semantic entities or constructions. For example, in the `English
-Resource Grammar <http://www.delph-in.net/erg/>`_, `_mouse_n_1` is the
-predicate for the word *mouse*, but it is underspecified for lexical
-semantics---it could be an animal, a computer's pointing device, or
-something else. When two nouns are compounded, as in *mouse pad*,
-the `compound` predicate is used on the predication that links them.
+Semantic predicates.
+
+Semantic predicates are atomic symbols representing semantic entities
+or constructions. For example, in the `English Resource Grammar
+<http://www.delph-in.net/erg/>`_, `_mouse_n_1` is the predicate for
+the word *mouse*, but it is underspecified for lexical semantics---it
+could be an animal, a computer's pointing device, or something
+else. Another example from the ERG is `compound`, which is used to
+link two compounded nouns, such as for *mouse pad*.
 
 There are two main categories of predicates: **abstract** and
 **surface**.  In form, abstract predicates do not begin with an
 underscore and in usage they often correspond to semantic
 constructions that are not represented by a token in the input, such
 as the `compound` example above. Surface predicates, in contrast, are
-the semantic representation of surface tokens, such as the
-`_mouse_n_1` example above. In form, they must always begin with a
+the semantic representation of surface (i.e., lexical) tokens, such as
+the `_mouse_n_1` example above. In form, they must always begin with a
 single underscore, and have two or three components: lemma,
-part-of-speech, and sense.
+part-of-speech, and (optionally) sense.
 
 .. seealso::
   - The DELPH-IN wiki about predicates:
     http://moin.delph-in.net/PredicateRfc
 
-In PyDelphin (as of `v1.0.0`_) predicates are simple strings, and this
-module provides functions to analyze (:func:`split`), create
-(:func:`create`), normalize (:func:`normalize`), and validate
+In DELPH-IN there is the concept of "real predicates" which are
+surface predicates decomposed into their lemma, part-of-speech, and
+sense, but in PyDelphin (as of `v1.0.0`_) predicates are always simple
+strings. However, this module has functions for composing and
+decomposing predicates from/to their components (the :func:`create`
+and :func:`split` functions, respectively). In addition, there are
+functions to normalize (:func:`normalize`) and validate
 (:func:`is_valid`, :func:`is_surface`, :func:`is_abstract`) predicate
 symbols.
 
