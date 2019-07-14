673504fbd5cb832dd6192c3e2d36cd8eacc73a45
Michael Wayne Goodman <goodman.m.w@gmail.com>
2019-07-14 09:18:11 +0100

    Add some delphin.scope tests and stubs

--- tests/scope_test.py
+++ tests/scope_test.py
@@ -2,6 +2,34 @@
 from delphin import scope
 
 
+class TestUnderspecifiedScope(object):
+    def test_init(self):
+        us = scope.UnderspecifiedScope([], [], [])
+        assert tuple(us) == (set(), {}, {})
+        us = scope.UnderspecifiedScope(
+            ['e2'], {}, {'h1': scope.UnderspecifiedScope(['x4'])})
+        assert tuple(us) == (
+            {'e2'}, {}, {'h1': scope.UnderspecifiedScope(['x4'], {}, {})})
+        assert us.ids == set(['e2'])
+        assert us.lheqs == {}
+        assert us.qeqs == {'h1': scope.UnderspecifiedScope(['x4'], {}, {})}
+
+    def test__repr__(self):
+        # just test for errors
+        repr(scope.UnderspecifiedScope(['x4'], {}, {}))
+        repr(scope.UnderspecifiedScope(
+            ['x4'], {}, {'h5': scope.UnderspecifiedScope('x6')}))
+
+    def test__contains__(self):
+        us = scope.UnderspecifiedScope([], [], [])
+        assert 'x5' not in us
+        us = scope.UnderspecifiedScope(
+            ['e2'], {}, {'h1': scope.UnderspecifiedScope(['x4'])})
+        assert 'e2' in us
+        assert 'x4' in us
+        assert 'h1' not in us
+
+
 def test_conjoin():
     assert scope.conjoin(
         {'h0': [], 'h1': ['e2']}, []) == {'h0': [], 'h1': ['e2']}
@@ -17,3 +45,11 @@ def test_conjoin():
                          [('h0', 'h1'), ('h1', 'h2'), ('h2', 'h3')])
     assert len(conj) == 1
     assert {'e2', 'x4', 'q5'} == set(next(iter(conj.values())))
+
+
+def test_tree_fragments():
+    pass
+
+
+def test_representatives():
+    pass
